aa8a87871261a6711112985254ba01b5a8950e77
albertsebastian1 <albertsebastian1@gmail.com>
2022-06-21 12:19:33 +1000

    test case intergartion and readme

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+func GetMethod(c *gin.Context) {
+	fmt.Println("\napi.go 'GetMethod' called")
+	message := "GetMethod called"
+	c.JSON(http.StatusOK, message)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "Hello, World")
 	})
 	fmt.Printf("Server running (port=80), route: http://localhost\n")
 	if err := http.ListenAndServe(":80", nil); err != nil {
